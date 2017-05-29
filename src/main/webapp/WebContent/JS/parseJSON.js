07ad414c3e029730b1e498f297a391f2339d1603
Peter <Peter@172.16.49.153>
2017-05-29 14:38:28 +0200

    JSON is now succesfully parsed and HTML result is displayed

--- src/main/webapp/WebContent/JS/parseJSON.js
+++ src/main/webapp/WebContent/JS/parseJSON.js
@@ -1,8 +1,31 @@
-window.onload = function() {
+/* window.onload = function() {
 	//Lese JSON-Datei ein
-	var jsonString = '{[{"type": "title","content": "Page Title"},{"type": "paragraph","content": "Bla"}]}';
+	var jsonString = '[{"type": "title","content": "Page Title"},{"type": "paragraph","content": "Bla"}]';
 	var json = JSON.parse(jsonString);
 	for (var i = 0;i < json.length; i++){
 		alert (json[i][type]);
 	}
-};
+}; */
+
+function parse(){
+	var jsonString = document.getElementById("json").value;
+	var json = JSON.parse(jsonString);
+	var output = "";
+	for (var i = 0; i < json.length; i++){
+		var type = json[i]["type"];
+		//alert (type);
+		switch (type){
+		case "title": 
+			output = output + '<h1>' + json[i]["content"]+ '</h1><br>';
+			break;
+		case "subtitle": 
+			output = output + '<h3>' + json[i]["content"]+ '</h3><br>';
+			break;	
+		case "paragraph":
+			output = output + '<p>' + json[i]["content"]+ '</p><br>';
+			break;
+		}
+	}
+	//alert (output);
+	document.getElementById("results").innerHTML = output;
+}
