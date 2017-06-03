aa23b8051454a5c2c066c630d7d2c51e03f76460
Peter <Peter@PK-Dell6500>
2017-06-03 15:02:01 +0200

    Implemented Servlet in testpage

--- src/main/webapp/WebContent/JS/parseJSON.js
+++ src/main/webapp/WebContent/JS/parseJSON.js
@@ -8,24 +8,44 @@
 }; */
 
 function parse(){
-	var jsonString = document.getElementById("json").value;
-	var json = JSON.parse(jsonString);
+	var step_id = document.getElementById("step_id").value;
 	var output = "";
-	for (var i = 0; i < json.length; i++){
-		var type = json[i]["type"];
-		//alert (type);
-		switch (type){
-		case "title": 
-			output = output + '<h1>' + json[i]["content"]+ '</h1><br>';
-			break;
-		case "subtitle": 
-			output = output + '<h3>' + json[i]["content"]+ '</h3><br>';
-			break;	
-		case "paragraph":
-			output = output + '<p>' + json[i]["content"]+ '</p><br>';
-			break;
+	$
+	.ajax({
+		type : "POST",
+		url : "get_json",
+		data : {
+			stepid : step_id,
+		},
+		success : function(result) {
+			alert(result);
+			if (result == "stepNotFound"){
+				alert("Der angegebene Schritt wurde nicht gefunden.");
+			} else if (result == "databaseError"){
+				alert("Ein Datenbankfehler ist aufgetreten.");
+			} else{
+				var json = JSON.parse(result);
+				for (var i = 0; i < json.length; i++){
+					var type = json[i]["type"];
+					//alert (type);
+					switch (type){
+					case "title": 
+						output = output + '<h1>' + json[i]["content"]+ '</h1><br>';
+						break;
+					case "subtitle": 
+						output = output + '<h3>' + json[i]["content"]+ '</h3><br>';
+						break;	
+					case "paragraph":
+						output = output + '<p>' + json[i]["content"]+ '</p><br>';
+						break;
+					}
+				}
+			}
+			
+		},
+		error : function(result) {
+			alert('Ein Fehler ist aufgetreten.');
 		}
-	}
-	//alert (output);
+	});
 	document.getElementById("results").innerHTML = output;
 }
