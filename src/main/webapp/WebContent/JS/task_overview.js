304ae4c132712327a242dce7ad75ecb0aa598130
Peter <Peter@172.16.49.153>
2017-06-19 15:29:21 +0200

    Debugging

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -11,11 +11,11 @@ function getList(){
 		data : {
 		},
 		success : function(result) {
-			result = result.substring(0, result.length - 1);
 			instances = result.split(",");
 			for (var i = 0; i < instances.length; i++){
 				singleInstance = instances[i].split("|");
 				output = output + "<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>";
+				alert("<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>");
 			}
 		},
 		error : function(result) {
