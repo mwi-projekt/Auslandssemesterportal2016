0ad7397ebb8ab0e81851f7db0df02f9a98ccf846
Peter <Peter@172.16.49.153>
2017-06-19 13:18:04 +0200

    Added task overview script and updated page

---
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	getList();
+});
+
+function getList(){
+	output = "<table><tr><td>Name</td><td>Vorname</td><td>Uni</td><td>Prüfen</td></tr>";
+	$
+	.ajax({
+		type : "GET",
+		url : "getTasks",
+		data : {
+		},
+		success : function(result) {
+			result = result.substring(0, result.length - 1);
+			instances = result.split(",");
+			for (var i = 0; i < instances.length; i++){
+				singleInstance = instances[i].split("|");
+				output = output + "<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>";
+			}
+		},
+		error : function(result) {
+			swal("Ein Fehler ist aufgetreten","error");
+		}
+	});
+	output = output + "</table>";
+	document.getElementById("resultList").innerHTML = output;
+}
