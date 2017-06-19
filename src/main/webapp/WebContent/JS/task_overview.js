109b87832fce85605a30352cbd767d81bf6e7d75
Peter <Peter@172.16.49.153>
2017-06-19 15:54:05 +0200

    Added button + Bootstrap formatting

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -10,11 +10,12 @@ function getList(){
 		data : {
 		},
 		success : function(result) {
-			output = "<table><tr><td>Name</td><td>Vorname</td><td>Uni</td><td>Prüfen</td></tr>";
+			output = "<table clases=\"table table-border table-hover\"><tr><td>Name</td><td>Vorname</td><td>Uni</td><td>Prüfen</td></tr>";
 			instances = result.split(",");
 			for (var i = 0; i < instances.length; i++){
 				singleInstance = instances[i].split("|");
-				output = output + "<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>";
+				output = output + "<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + 
+				'<button href="http://193.196.7.215:8080/Auslandssemesterportal/WebContent/task_detail.html?instance_id=' + singleInstance[0] + '&verify=true></button>' + "</td></tr>";
 				//alert("<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>");
 			}
 			output = output + "</table>";
