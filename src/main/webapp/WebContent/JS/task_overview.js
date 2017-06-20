98c56d626acaa2e0b976999d63c0af5069dcb28c
Peter <Peter@172.16.49.153>
2017-06-20 13:02:39 +0200

    Admin task overview now operational

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -10,15 +10,16 @@ function getList(){
 		data : {
 		},
 		success : function(result) {
-			output = "<table clases=\"table table-border table-hover\"><tr><td>Name</td><td>Vorname</td><td>Uni</td><td>Prüfen</td></tr>";
-			instances = result.split(",");
+			result = result.substring(0, result.length - 1);
+			output = '<table class="table table-hover table-bordered"><thead><tr><th>Name</th><th>Vorname</th><th>Uni</th><th>Prüfen</th></tr></thead><tbody>';
+			instances = result.split(";");
 			for (var i = 0; i < instances.length; i++){
 				singleInstance = instances[i].split("|");
 				output = output + "<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + 
-				'<button href="http://193.196.7.215:8080/Auslandssemesterportal/WebContent/task_detail.html?instance_id=' + singleInstance[0] + '&verify=true></button>' + "</td></tr>";
+				'<button class="btn btn-default" onclick="location.href=\'http://193.196.7.215:8080/Auslandssemesterportal/WebContent/task_detail.html?instance_id=' + singleInstance[0] + '&verify=true\'">Details</button>' + "</td></tr>"
 				//alert("<tr><td>" + singleInstance[1] + "</td><td>" + singleInstance[2] + "</td><td>" + singleInstance[3] + "</td><td>" + singleInstance[0] + "</td></tr>");
 			}
-			output = output + "</table>";
+			output = output + "</tbody></table>";
 			document.getElementById("resultList").innerHTML = output;
 		},
 		error : function(result) {
