2d61c74f4a96b76a51da04ae1362fd81cca17791
Peter <Peter@PK-Dell6500>
2017-06-25 15:13:03 +0200

    Added blob. Object URL is now created. File still invalid according to pdf.js

--- src/main/webapp/WebContent/JS/file_download.js
+++ src/main/webapp/WebContent/JS/file_download.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
 			key : filename
 		},
 		success : function(result) {
-			location.href=result;
+			var blob=new Blob([result],{type: 'application/pdf'});
+			var url = window.URL.createObjectURL(blob);
+			location.replace(url);
 		},
 		error : function(result) {
 			alert('Ein Fehler ist aufgetreten');
