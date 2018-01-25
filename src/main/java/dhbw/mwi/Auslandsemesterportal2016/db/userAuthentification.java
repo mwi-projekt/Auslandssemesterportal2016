806f8121bd6a96d44194201861be8518444c0a64
thomas-br <thomas.bruckner@sap.com>
2018-01-25 21:24:31 +0100

    set method to public

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/userAuthentification.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/userAuthentification.java
@@ -4,16 +4,16 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
-public class userAuthentification 
+public class userAuthentification
 {
 	//Rolle für User: 1 = Admin ; 2 = Mitarbeiter ; 3 = Student ; 0 = Fehler
-	static int isUserAuthentifiedByCookie(HttpServletRequest request)
+	public static int isUserAuthentifiedByCookie(HttpServletRequest request)
 	{
 		String sessionId = null, mail = null;
 		Cookie[] cookies = request.getCookies();
 		int role = 0;
 		//check if there are any cookies at all and then if session id has been set, meaning user is logged in
-		if(cookies != null) 
+		if(cookies != null)
 		{
 			for (int i = 0; i < cookies.length; i++)
 			{
@@ -23,8 +23,8 @@ public class userAuthentification
 				if(cookies[i].getName() =="email")
 					mail = cookies[i].getValue();
 			}
-		
-		
+
+
 		//check if session-id was found in cookies
 			if(sessionId != null && mail != null)
 			{
