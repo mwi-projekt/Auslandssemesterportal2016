de866f6830e9228590312003e27b6c90cdf04dc3
thomas-br <thomas.bruckner@sap.com>
2018-01-25 22:50:41 +0100

    implemented workaround until userAuthentication class is fixed by Flo

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetAdminTasksServlet.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetAdminTasksServlet.java
@@ -25,11 +25,18 @@ public class GetAdminTasksServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      int rolle = userAuthentification.isUserAuthentifiedByCookie(request);
+      //Workaround until Florian fixed his Method
+      int rolle = -1;
       String sessionID = request.getCookies()[1].getValue();
       String mail = request.getCookies()[0].getValue();
+      if(SQL_queries.checkUserSession(sessionID, mail)){
+        rolle = SQL_queries.getRoleForUser(mail);
+      }
+
+      //int rolle = userAuthentification.isUserAuthentifiedByCookie(request);
+
       if(rolle!=1 && rolle!=2){
-        response.sendError(401,""+SQL_queries.checkUserSession(sessionID, mail)+" "+SQL_queries.getRoleForUser(mail)+" Rolle: "+rolle);
+        response.sendError(401,""++" "++" Rolle: "+rolle);
       }
       else{
         response.setCharacterEncoding("UTF-8");
