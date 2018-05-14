4bd0f087b4b3dad1a38deda15e324c2d8d7116b8
Florian Sass <sass005@cougars.csusm.edu>
2018-05-14 09:44:49 +0200

    Added Test Package

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/test/GetHTMLFileServletTest.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/test/GetHTMLFileServletTest.java
@@ -1,4 +1,4 @@
-package dhbw.mwi.Auslandsemesterportal2016.servlets;
+package dhbw.mwi.Auslandsemesterportal2016.test;
 
 import static org.junit.Assert.*;
 
@@ -9,36 +9,49 @@ import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import dhbw.mwi.Auslandsemesterportal2016.db.userAuthentification;
+import dhbw.mwi.Auslandsemesterportal2016.servlets.GetHtmlFileServlet;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class GetHTMLFileServletTest  extends Mockito
 {
-	
 	private HttpServletRequest request;
 	private HttpServletResponse response;
 	private GetHtmlFileServlet servlet;
+	private userAuthentification userAuth;
 	
 	@Test
 	public void requestForNonExistingPageShouldFail()  throws Exception
 	{
-		String requestedPage = "xyz";
-		when(request.getParameter("page")).thenReturn(requestedPage);
+		//Mock Ups
+		when(request.getParameter("page")).thenReturn("xyz");
 		when(request.getParameter("accessLevel")).thenReturn("0");
+		
+//		doReturn(1).when(userAuth).isUserAuthentifiedByCookie(request);
 		when(userAuthentification.isUserAuthentifiedByCookie(request)).thenReturn(1);
 		
+		//Do Something
 		servlet.doGet(request, response);
+		
+		//Check Success
 		assertEquals(response.toString(), 403, response.getStatus());
 	}
 
 	@Test
 	public void requestForTestPageShouldSucceed() throws Exception
 	{
+		//Mock Ups
 		when(request.getParameter("page")).thenReturn("student");
-		when(request.getCookies()).thenReturn(new Cookie[]{new Cookie("mail","a.a@a.com"), new Cookie("sessionID","XXX")});
+		when(userAuthentification.isUserAuthentifiedByCookie(request)).thenReturn(1);
+		
+		//Do Something
 		servlet.doGet(request, response);
+		
+		//Check Success
 		assertEquals(response.toString(), 200, response.getStatus());
 	}
 	
@@ -48,6 +61,7 @@ public class GetHTMLFileServletTest  extends Mockito
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
 		servlet = new GetHtmlFileServlet();
+		userAuth = mock(userAuthentification.class);
 	}
 
 	@After
@@ -56,5 +70,6 @@ public class GetHTMLFileServletTest  extends Mockito
 		request = null;
 		response = null;
 		servlet = null;
+		userAuth = null;
 	}
 }
