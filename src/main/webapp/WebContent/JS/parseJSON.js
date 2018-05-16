fb34b29b0e19e8bc661b6b508327db825fe6d28e
alexa <alexa@172.16.50.239>
2018-05-16 13:25:18 +0200

    test schottland

--- src/main/webapp/WebContent/JS/parseJSON.js
+++ src/main/webapp/WebContent/JS/parseJSON.js
@@ -72,7 +72,7 @@ function parse(){
 				type : "GET",
 				url : "processmodel/get",
 				data : {
-					model: 'studentBewerben',
+					model: 'schottland',
 					step : step_id
 				},
 				success : function(result) {
