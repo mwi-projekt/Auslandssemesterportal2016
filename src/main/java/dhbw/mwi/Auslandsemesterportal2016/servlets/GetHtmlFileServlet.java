f540eeb1b8642ba9ac5014bc18026affb00aea61
Florian Sass <sass005@cougars.csusm.edu>
2018-05-20 13:32:15 +0200

    500 fixed

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetHtmlFileServlet.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetHtmlFileServlet.java
@@ -68,32 +68,44 @@ public class GetHtmlFileServlet extends HttpServlet
 			}
 		}
 		//wurde ein erlaubtes Dokument gefunden?
-		if(highestDocumentLevelFound >= 0)
+		try
 		{
-			response.setStatus(HttpServletResponse.SC_ACCEPTED);
-			RequestDispatcher view = request.getServletContext().getRequestDispatcher(HTML_FOLDER[highestDocumentLevelFound]+requestedPage+".html");
-			view.include(request, response);
-			return;
-		}
-		//es wurde kein erlaubtes Dokument gefunden
-		else
-		{
-			//es wurde ein verbotenes Dokument gefunden
-			if(forbiddenDocumentFound)
+			
+			if(highestDocumentLevelFound >= 0)
 			{
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-		                "Sie haben keine Berechtigung auf diese Seite zuzugreifen!");
+				
+				response.setStatus(HttpServletResponse.SC_ACCEPTED);
+				RequestDispatcher view = request.getServletContext().getRequestDispatcher(HTML_FOLDER[highestDocumentLevelFound]+requestedPage+".html");
+				view.include(request, response);
 				return;
 			}
-			//es wurde nichts gefunden
+			//es wurde kein erlaubtes Dokument gefunden
 			else
 			{
-				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-				response.sendError(HttpServletResponse.SC_NOT_FOUND,
-		                "Seite wurde nicht gefunden!");
-				return;
+				//es wurde ein verbotenes Dokument gefunden
+				if(forbiddenDocumentFound)
+				{
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+			                "Sie haben keine Berechtigung auf diese Seite zuzugreifen!");
+					return;
+				}
+				//es wurde nichts gefunden
+				else
+				{
+					response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+					response.sendError(HttpServletResponse.SC_NOT_FOUND,
+			                "Seite wurde nicht gefunden!");
+					return;
+				}
 			}
 		}
+		catch(Exception e)
+		{
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		}
+		
+		
 	}
 }
