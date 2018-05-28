bd0255f44cc6271e731d59f73e68e73df5909189
Florian Sass <sass005@cougars.csusm.edu>
2018-05-28 11:47:18 +0200

    Revert "Access Level changed"
    This reverts commit 178fa330c41d4d9ee34e13b9aa234808cea8a4d0.

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -1,7 +1,7 @@
 $(document)
 		.ready(
 				function() {
-					if (sessionStorage['rolle'] === '2') {
+					if (sessionStorage['rolle'] === '3') {
 						swal(
 								{
 									title : "Fehler!",
