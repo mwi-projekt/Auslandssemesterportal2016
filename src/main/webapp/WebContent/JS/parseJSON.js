df06d1d10d084f6334eafafcd3f7fb966e1f2e90
alexa <alexa@DESKTOP-ARCP87F.fritz.box>
2018-06-03 16:57:26 +0200

    var uni

--- src/main/webapp/WebContent/JS/parseJSON.js
+++ src/main/webapp/WebContent/JS/parseJSON.js
@@ -71,7 +71,7 @@ function parse(){
 				type : "GET",
 				url : "processmodel/get",
 				data : {
-					model: schottland,
+					model: uni,
 					step : step_id
 				},
 				success : function(result) {
