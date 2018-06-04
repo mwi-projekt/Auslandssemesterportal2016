8a7e816fc39b1597c902b90c838e20429041409a
thomas-br <thomas.bruckner@me.com>
2018-06-04 15:17:24 +0200

    Test for Camunda Mail Sending

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/login_db.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/login_db.java
@@ -882,7 +882,7 @@ public class login_db extends HttpServlet implements TaskListener, JavaDelegate 
             // Automatic Mail Server Properties
 
             try {
-        	Message message = Util.getEmailMessage("mwiausland@gmail.com", "Akademisches Auslandsamt Registrierung");
+        	Message message = Util.getEmailMessage("info@thomasbruckner.de", "Akademisches Auslandsamt Registrierung");
 
                 message.setContent("Sehr geehrte Frau Dreischer," + "\n" + "\n"
 					+ "ein weiterer Student hat das Bewerbungsfomular für ein Auslandssemester abgeschlossen." + "\n"
