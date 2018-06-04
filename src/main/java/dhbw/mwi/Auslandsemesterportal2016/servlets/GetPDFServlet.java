5a1de272a281d671fb2b3b14d1af8b2ffa584977
Florian Sass <sass005@cougars.csusm.edu>
2018-06-04 10:01:54 +0200

    Fix for PDF Servlet

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetPDFServlet.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetPDFServlet.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import dhbw.mwi.Auslandsemesterportal2016.db.userAuthentification;
 
-@WebServlet(name = "GetHtmlFileServlet", urlPatterns = {"/WebContent/anmeldeformular",
+@WebServlet(name = "GetHtmlFileServlet", urlPatterns = 
+		{"/WebContent/Anmeldeformular",
 		"/WebContent/daad_formular"})
 
 
 public class GetPDFServlet extends HttpServlet
 {
-	private static final String PDF_FOLDER = "docs";
+	private static final String PDF_FOLDER = "/WebContent/docs";
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
 	{
