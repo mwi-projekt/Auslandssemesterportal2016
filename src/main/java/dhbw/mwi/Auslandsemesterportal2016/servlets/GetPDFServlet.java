a37c1350fa9f36206c8edb180a51c8c7eb35e57c
Florian Sass <sass005@cougars.csusm.edu>
2018-06-04 09:58:05 +0200

    PDF Servlet added

---
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/servlets/GetPDFServlet.java
@@ -0,0 +1,33 @@
+package dhbw.mwi.Auslandsemesterportal2016.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dhbw.mwi.Auslandsemesterportal2016.db.userAuthentification;
+
+@WebServlet(name = "GetHtmlFileServlet", urlPatterns = {"/WebContent/anmeldeformular",
+		"/WebContent/daad_formular"})
+
+
+public class GetPDFServlet extends HttpServlet
+{
+	private static final String PDF_FOLDER = "docs";
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		
+		String requestedPage = request.getServletPath();
+		requestedPage = requestedPage.substring(11, requestedPage.length());
+		
+		response.setStatus(HttpServletResponse.SC_ACCEPTED);
+		RequestDispatcher view = request.getServletContext().getRequestDispatcher(PDF_FOLDER+requestedPage+".pdf");
+		view.include(request, response);
+		return;
+	}
+}
