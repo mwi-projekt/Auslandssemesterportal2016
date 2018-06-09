126888ab3762f1f83effa976c1f37dc01fd7570d
uemran <uemran.celen@gmx.de>
2018-06-09 12:29:14 +0200

    -

--- src/main/webapp/WebContent/JS/bewerbungsportal.js
+++ src/main/webapp/WebContent/JS/bewerbungsportal.js
@@ -71,17 +71,17 @@ var main = function() {
 						//Anlegen der Buttons
 						if ((instance_info[2] === "Abgeschlossen")||(instance_info[2] === "Auf Rückmeldung warten")){
 							//Übersicht
-							tabelle = tabelle + '<span class="glyphicon glyphicon-list" title="Übersicht" onclick="location.href=\'task_detail.html?instance_id=' + instance_info[0] + '\'"></span>';
+							tabelle = tabelle + '<td><span class="glyphicon glyphicon-list" title="Übersicht" onclick="location.href=\'task_detail.html?instance_id=' + instance_info[0] + '\'"></span></td>';
 						} else if (instance_info[2] === "Daten prüfen"){
 								//Übersicht
-								tabelle = tabelle + '<span class="glyphicon glyphicon-list" title="Übersicht" onclick="location.href=\'task_detail.html?instance_id=' + instance_info[0] + '&send_bew=true\'"></span>';
+								tabelle = tabelle + '<td><span class="glyphicon glyphicon-list" title="Übersicht" onclick="location.href=\'task_detail.html?instance_id=' + instance_info[0] + '&send_bew=true\'"></span></td>';
 								//Prozess löschen
-								tabelle = tabelle + '<span uni="' + instance_info[1] + '" class="glyphicon glyphicon-trash btn-delete" title="Löschen" rid="' + (i+1) + '"></span>';
+								tabelle = tabelle + '<td><span uni="' + instance_info[1] + '" class="glyphicon glyphicon-trash btn-delete" title="Löschen" rid="' + (i+1) + '"></span></td>';
 						} else {
 							//Fortsetzen
-							tabelle = tabelle + '<span class="glyphicon glyphicon-circle-arrow-right" title="fortsetzen" onclick="location.href=\'bewerben.html?instance_id=' + instance_info[0] + '&uni='+instance_info[1]+'\'"></span>';
+							tabelle = tabelle + '<td><span class="glyphicon glyphicon-circle-arrow-right" title="fortsetzen" onclick="location.href=\'bewerben.html?instance_id=' + instance_info[0] + '&uni='+instance_info[1]+'\'"></span></td>';
 							//Prozess löschen
-							tabelle = tabelle + '<span uni="' + instance_info[1] + '" class="glyphicon glyphicon-trash btn-delete" title="Löschen" rid="' + (i+1) + '"></span>'
+							tabelle = tabelle + '<td><span uni="' + instance_info[1] + '" class="glyphicon glyphicon-trash btn-delete" title="Löschen" rid="' + (i+1) + '"></span></td>'
 						}
 						tabelle = tabelle + '</td></tr>'
 					}
