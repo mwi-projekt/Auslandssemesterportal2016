04efad2f5b66433af1f75369d04f64d59bd1d6f7
uemran <uemran.celen@gmx.de>
2018-06-10 22:34:48 +0200

    -

--- src/main/webapp/WebContent/JS/main.js
+++ src/main/webapp/WebContent/JS/main.js
@@ -1102,11 +1102,11 @@ var main = function() {
 															+ auslesen[i + 6]
 															+ '</td><td class="matrikelnr">'
 															+ auslesen[i + 7]
-															+ '</td><td><span class="btn glyphicon glyphicon-edit" id="edit'
+															+ '</td><td><button class="glyphicon glyphicon-edit" id="edit'
 															+ count
-															+ '"></span></td><td><span class="glyphicon glyphicon-trash delete-button" data-matrikel="'+ auslesen[i + 7].trim() +'" id="delete'
+															+ '"></button></td><td><button class="glyphicon glyphicon-trash delete-button" data-matrikel="'+ auslesen[i + 7].trim() +'" id="delete'
                             + count
-                            + '"></span></td></tr>';
+                            + '"></button></td></tr>';
 													if (even === 'even') {
 														even = 'odd';
 													} else {
