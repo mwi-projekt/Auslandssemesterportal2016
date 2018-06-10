53150ede4c8e2f772136246834496c5078c43582
uemran <uemran.celen@gmx.de>
2018-06-10 22:03:49 +0200

    -

--- src/main/webapp/WebContent/JS/admin-process-overview.js
+++ src/main/webapp/WebContent/JS/admin-process-overview.js
@@ -37,5 +37,56 @@ $(document).ready(function () {
         },
     });
 
+	$('.imgSlider').css('background-image', 'url(images/pan' + back + '.jpg)');
+	loadPortalInfo();
+	loop();
+	// Überprüfen ob jemand eingeloggt ist
+	if (isEmpty(sessionStorage['User']) === true
+			|| sessionStorage['User'] === 'undefined') {
+		$('.logoutFenster').hide();
+	} else {
+		$('.loginFenster').hide();
+		$('.logFenster').hide();
+		$('.loginButton').hide();
+		$('.regButton').hide();
+		$('.portalInfo').hide();
+		$('.logoFenster').show();
+		$('.logoutFenster').show();
+		$('.nutzerName').text(sessionStorage['User']);
+		if (sessionStorage['rolle'] === "2") {
+			$('.cms').show();
+			$('.nonCms').show();
+			$('.Admin').hide();
+			$('.Mitarbeiter').show();
+			$('.portalInfo').show();
+		} else if (sessionStorage['rolle'] === "1") {
+			if (sessionStorage['verwaltung'] === "0"
+					|| sessionStorage['verwaltung'] === undefined) {
+				$('#normalBereich').hide();
+				$('#adminBereich').show();
+			} else if (sessionStorage['verwaltung'] === "1") {
+				$('#normalBereich').show();
+				$('.cms').show();
+				$('#adminBereich').hide();
+				$('.Admin').show();
+				$('.Mitarbeiter').hide();
+			} else if (sessionStorage['verwaltung'] === "2") {
+				$('#adminBereich').hide();
+				$('#nutzerVerwaltung').show();
+				$('#normalBereich').hide();
+			}
+			$('.Mitarbeiter').hide();
+			$('.portalInfo').show();
+		} else {
+			$('.cms').hide();
+			$('.nonCms').show();
+			$('.eingeloggt').css('display', 'inline');
+		}
+	}
+
+	for (var i = 1; i < 10; i++) {
+		$('.angCont' + i).hide();
+		$('.c' + i + '1').show();
+	}
 
 });
