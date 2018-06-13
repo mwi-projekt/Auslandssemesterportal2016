5d0c6462dd7008c48fe5d13541b734e9247c9968
ChristinaHoffmann <chrhoffmann@online.de>
2018-06-13 11:51:00 +0200

    test

--- src/main/webapp/WebContent/JS/task_detail.js
+++ src/main/webapp/WebContent/JS/task_detail.js
@@ -83,7 +83,7 @@ function parse() {
 					collapsible = steps[k].split("|");
 					stepName = collapsible[0]; // Name des aktiven
 					// Prozessschrittes
-					console.log(stepName);
+					
 					if (collapsible[1].search("id") != -1) {
 						var innerOutput = "";
 						var json = JSON.parse(decodeURI(collapsible[1]));
@@ -143,6 +143,7 @@ function parse() {
 						}
 
 						if (innerOutput != '') {
+						console.log(stepName);
 							output = output +
 								'<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#collapse' +
 								k + '">' + stepName + '</a></h4></div>'; // Header
