99d5b878531cd568225ef288effbd8e294c65a87
ChristinaHoffmann <chrhoffmann@online.de>
2018-06-13 11:49:17 +0200

    Revert "test"
    This reverts commit 95895fee29d6e9a6d2599d1bed69a90dd24c52f1.

--- src/main/webapp/WebContent/JS/task_detail.js
+++ src/main/webapp/WebContent/JS/task_detail.js
@@ -145,7 +145,7 @@ function parse() {
 						if (innerOutput != '') {
 							output = output +
 								'<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#collapse' +
-								k + '">' + Persönliche Daten + '</a></h4></div>'; // Header
+								k + '">' + stepName + '</a></h4></div>'; // Header
 							// des
 							// Accordions
 							output = output +
