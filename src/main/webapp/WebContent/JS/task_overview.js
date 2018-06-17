9e4952805c95e878bc0a867ad6037e93553f7a79
fabianhugler <fabianhugler@gmx.de>
2018-06-17 21:00:44 +0200

    task als id

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -86,18 +86,18 @@ function getList() {
 						if (output === ""){
 							output = "<h2>Aktuell gibt es keine Bewerbungen, die überprüft werden müssen</h2>";
 						} else {
-							output = '<table class="table table-striped table-bordered"><thead><tr><th>Name</th><th>Vorname</th><th>Kurs</th><th>Heimatuniversität</th><th>Partneruniversität</th><th>Prüfen</th></tr></thead><tbody>' +
+							output = '<table id="task" class="table table-striped table-bordered"><thead><tr><th>Name</th><th>Vorname</th><th>Kurs</th><th>Heimatuniversität</th><th>Partneruniversität</th><th>Prüfen</th></tr></thead><tbody>' +
 							output + "</tbody></table>";
 						}
 						if (completed === ""){
 							completed = "<h2>Es gibt noch keine abgeschlossenen Bewerbungen</h2>";
 						} else {
-							completed = '<table class="table table-striped table-bordered"><thead><tr><th>Name</th><th>Vorname</th><th>Heimatuniversität</th><th>Kurs</th><th>Partneruniversität</th><th>Prüfen</th></tr></thead><tbody>' +
+							completed = '<table id="task" class="table table-striped table-bordered"><thead><tr><th>Name</th><th>Vorname</th><th>Heimatuniversität</th><th>Kurs</th><th>Partneruniversität</th><th>Prüfen</th></tr></thead><tbody>' +
 							completed + "</tbody></table>";
 						}
 
 						$(document).ready(function() {
-    					$('.table').DataTable();
+    					$('#task').DataTable();
 						} );
 
 					}
