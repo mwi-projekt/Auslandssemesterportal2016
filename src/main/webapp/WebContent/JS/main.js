fc7417a67696ec979d272bbdcea548b127d7a8d2
alexdide97 <alexander.dideban@gmail.com>
2018-06-24 21:55:39 +0200

    test

--- src/main/webapp/WebContent/JS/main.js
+++ src/main/webapp/WebContent/JS/main.js
@@ -523,7 +523,7 @@ var main = function() {
 								}
 								break;
 							}
-						} else if ($(this).text() === "&aumlnderungen speichern") {
+						} else if ($(this).text() === "Änderungen speichern") {
 							var klasse = $(this).attr('class');
 							klasse = klasse.replace('btn', "");
 							klasse = klasse.replace('cmsBtn', "");
