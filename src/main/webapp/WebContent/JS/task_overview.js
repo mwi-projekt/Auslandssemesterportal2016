de7b62df74f319855fa603ca544c9c13d0ed95f6
uemran <uemran.celen@gmx.de>
2018-06-24 20:23:22 +0200

    -

--- src/main/webapp/WebContent/JS/task_overview.js
+++ src/main/webapp/WebContent/JS/task_overview.js
@@ -58,7 +58,7 @@ function getList() {
 										+ "</td><td>"
 										+ singleInstance[5]
 										+ "</td><td>"
-										+ '<button class="btn glyphicon glyphicon-list" name="Details" onclick="location.href=\'http://193.196.7.215:8080/Auslandssemesterportal/WebContent/task_detail.html?instance_id='
+										+ '<button class="btn glyphicon glyphicon-list" title="Details" onclick="location.href=\'http://193.196.7.215:8080/Auslandssemesterportal/WebContent/task_detail.html?instance_id='
 										+ singleInstance[0]
 										+ '&uni='
 										+ singleInstance[5]
