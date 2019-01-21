e41d7331429ff2d8b2eb274553d0c19e54a79f24
daniels9 <33021140+daniels9@users.noreply.github.com>
2019-01-21 14:15:02 +0100

    Änderungen an task_overview.js für Löschfunktion AAA-Mitarbeiter 2

--- src/main/webapp/WebContent/assets/js/pages/task_overview.js
+++ src/main/webapp/WebContent/assets/js/pages/task_overview.js
@@ -38,7 +38,7 @@ function getList() {
                             singleInstance.uni +
                             '&verify=true\'"> </button>' +
                             "</td><td>" +
-                            '<button class="btn glyphicon glyphicon-trash taskdelete" title="Delete" data-id="' + singleInstance.id + '"  id="taskdelete' +
+                            '<button class="btn glyphicon glyphicon-trash taskdelete" title="Delete" onclick="window.location.href=\'https://www.google.de/'  +
                             singleInstance.id +
                             '"></button>' +
                             "</td></tr>"
