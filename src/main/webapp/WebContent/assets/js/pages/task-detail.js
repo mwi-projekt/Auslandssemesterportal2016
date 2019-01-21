36118954e71215d04e8bedb1cd2be391414399fe
mschwandt <michaela.schwandt@cofinpro.de>
2019-01-21 12:31:31 +0100

    Felder sperren

--- src/main/webapp/WebContent/assets/js/pages/task-detail.js
+++ src/main/webapp/WebContent/assets/js/pages/task-detail.js
@@ -111,7 +111,7 @@ function parse() {
                                 innerOutput = innerOutput +
                                     '<div class="form-group"><div class="col-sm-offset-2 col-sm-10"><div class="checkbox"><label><input type="checkbox" id="' +
                                     json[i]["data"]["id"] + '"> ' +
-                                    json[i]["data"]["label"] + dis +
+                                    json[i]["data"]["label"] +
                                     ' </label></div></div></div>';
                                 idList.push(json[i]["data"]["id"]);
                                 typeList.push("boolean");
