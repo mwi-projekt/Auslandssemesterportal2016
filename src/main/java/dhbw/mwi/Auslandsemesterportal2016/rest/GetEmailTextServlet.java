1ef9d86491bf2fa3b740979d40e6866f90badca4
mschwandt <michaela.schwandt@cofinpro.de>
2019-05-13 11:34:23 +0200

    E Mail test

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/rest/GetEmailTextServlet.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/rest/GetEmailTextServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
 
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
@@ -38,64 +36,29 @@ public class GetEmailTextServlet extends HttpServlet {
 			String uni = runtime.getVariable(instanceID, "uni").toString();
 			String output = "";
 
-			JFrame frame = new JFrame();
-			JEditorPane ausgabe = new JEditorPane();
-			ausgabe.setContentType("text/html");
-
 			if (validation_result.equals("true")) {
 				// Text für erfolgreiche Bewerbung
-				/*
-				 * output = "Sehr geehrte/r Herr/Frau " + student_name + (",") + "\n" + "\n" +
-				 * "Herzlichen Glückwunsch! Ihre Bewerbung für das von Ihnen ausgewählte Auslandssemesterangebot an der Universität: "
-				 * + uni + " wurde erfolgreich an das Akademisches Auslandsamt versendet." +
-				 * "\n" + "\n" + "-- Platzhalter für Anmerkungen des Auslandsamts --" + "\n" +
-				 * "\n" + "\n" +
-				 * "Im nächsten Schritt wird sich ein Mitarbeiter zeitnah um die Bearbeitung Ihrer Bewerbung kümmern und	entscheiden, ob Sie in die engere Auswahl potentieller Bewerber kommen."
-				 * + "\n" +
-				 * "Sobald dieser Prozess abgeschlossen ist, werden wir Sie schnellstmöglich per Email über das Ergebnis informieren."
-				 * + "\n" + "\n" + "Mit freundlichen Grüßen," + "\n" + "\n" +
-				 * "Ihr Akademisches Auslandsamt";
-				 */
-				ausgabe.setText("<html>" + "Sehr geehrte/r Herr/Frau " + student_name + (",") + "<br>" + "<br>"
+				output = "Sehr geehrte/r Herr/Frau " + student_name + (",") + "\n" + "\n"
 						+ "Herzlichen Glückwunsch! Ihre Bewerbung für das von Ihnen ausgewählte Auslandssemesterangebot an der Universität: "
-						+ uni + " wurde erfolgreich an das Akademisches Auslandsamt versendet." + "<br>"
-						+ "<b> -- Platzhalter für Anmerkungen des Auslandsamts -- </b>" + "<br>" + "<br" + "<br>"
-						+ "Im nächsten Schritt wird sich ein Mitarbeiter zeitnah um die Bearbeitung Ihrer Bewerbung kümmern und entscheiden, ob Sie in die engere Auswahl potentieller Bewerber kommen."
-						+ "<br>"
+						+ uni + " wurde erfolgreich an das Akademisches Auslandsamt versendet." + "\n" + "\n"
+						+ "-- Platzhalter für Anmerkungen des Auslandsamts --" + "\n" + "\n" + "\n"
+						+ "Im nächsten Schritt wird sich ein Mitarbeiter zeitnah um die Bearbeitung Ihrer Bewerbung kümmern und	entscheiden, ob Sie in die engere Auswahl potentieller Bewerber kommen."
+						+ "\n"
 						+ "Sobald dieser Prozess abgeschlossen ist, werden wir Sie schnellstmöglich per Email über das Ergebnis informieren."
-						+ "<br>" + "<br>" + "Mit freundlichen Grüßen," + "<br>" + "<br>"
-						+ "Ihr Akademisches Auslandsamt" + "</html>.");
-
+						+ "\n" + "\n" + "Mit freundlichen Grüßen," + "\n" + "\n" + "Ihr Akademisches Auslandsamt";
 			} else {
 				// Text für abgelehnte Bewerbung
-				/*
-				 * output = "Sehr geehrte/r Herr/Frau " + student_name + (",") + "\n" + "\n" +
-				 * "Vielen Dank für Ihre eingereichte Bewerbung an der Universität: " + uni +
-				 * "\n" +
-				 * "Leider wurden nicht alle Daten vollständig und/oder korrekt eingegeben." +
-				 * "\n" + "\n" + "Folgende Problem hat sich ergeben: " + "\n " + "\n" +
-				 * " -- Platzhalter für Erläuterung des Problems -- " + "\n" + "\n" +
-				 * "Ihr Bewerbungsprozess wurde auf Anfang zurückgesetzt, damit Sie den Fehler beheben können."
-				 * + "\n" + "Wir bitten um Ihr Verständnis." + "\n" + "\n" +
-				 * "Mit freundlichen Grüßen," + "\n" + "\n" + "Ihr Akademisches Auslandsamt";
-				 * 
-				 */
-				ausgabe.setText("<html>" + "Sehr geehrte/r Herr/Frau " + student_name + (",") + "\n" + "\n"
+				output = "Sehr geehrte/r Herr/Frau " + student_name + (",") + "\n" + "\n"
 						+ "Vielen Dank für Ihre eingereichte Bewerbung an der Universität: " + uni + "\n"
 						+ "Leider wurden nicht alle Daten vollständig und/oder korrekt eingegeben." + "\n" + "\n"
-						+ "Folgende Problem hat sich ergeben:" + "\n "
-						+ " <strong> -- Platzhalter für Erläuterung des Problems -- </strong> " + "\n" + "\n"
+						+ "Folgende Problem hat sich ergeben: " + "\n " + "\n"
+						+ " -- Platzhalter für Erläuterung des Problems -- " + "\n" + "\n"
 						+ "Ihr Bewerbungsprozess wurde auf Anfang zurückgesetzt, damit Sie den Fehler beheben können."
 						+ "\n" + "Wir bitten um Ihr Verständnis." + "\n" + "\n" + "Mit freundlichen Grüßen," + "\n"
-						+ "\n" + "Ihr Akademisches Auslandsamt" + "</html>.");
-
+						+ "\n" + "Ihr Akademisches Auslandsamt";
 			}
 
-			// toClient.print(output);
-			ausgabe.setEditable(false);
-			frame.add(ausgabe);
-			frame.setVisible(true);
-			toClient.print(ausgabe.getText());
+			toClient.print(output);
 
 		}
 	}
