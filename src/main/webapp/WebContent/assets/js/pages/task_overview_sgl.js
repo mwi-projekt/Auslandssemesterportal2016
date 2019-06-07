8a6c7f41ef24263c4311ff5b6fb8461b93231bae
daniels9 <33021140+daniels9@users.noreply.github.com>
2019-06-07 10:00:57 +0200

    Update task_overview_sgl.js

--- src/main/webapp/WebContent/assets/js/pages/task_overview_sgl.js
+++ src/main/webapp/WebContent/assets/js/pages/task_overview_sgl.js
@@ -63,8 +63,8 @@ function getList() {
                     } 
                    
                 }
-                
-                console.log("TEST2" + output);
+            }
+               
                 if (output === "") {
                 	console.log(output);
                     output = "<h2>Aktuell gibt es keine Bewerbungen, die überprüft werden müssen</h2>";
@@ -83,14 +83,14 @@ function getList() {
                 $(document).ready(function () {
                     $('.table').DataTable();
                 });
-            }
+           // }
             
             console.log("TEST3" + output);
             
-            if (output === "") {
+           /* if (output === "") {
             	console.log(output);
                 output = "<h2>Aktuell gibt es keine Bewerbungen, die überprüft werden müssen</h2>";
-            } 
+            } */
            
             document.getElementById("resultList").innerHTML = '<h1>Zu validierende Bewerbungen</h1>' + output + '<br><h1>Abgeschlossene Bewerbungen</h1>' + completed; 
         },
