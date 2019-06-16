1120f53fafc1e5985904b39514dc80c766beee7a
daniels9 <33021140+daniels9@users.noreply.github.com>
2019-06-16 21:38:31 +0200

    Update bewerbungsportal.js

--- src/main/webapp/WebContent/assets/js/pages/bewerbungsportal.js
+++ src/main/webapp/WebContent/assets/js/pages/bewerbungsportal.js
@@ -43,14 +43,13 @@ $(document).ready(function () {
                     if (splitUni.indexOf(result.data[l].uniTitel) != -1) continue;
                     popUpHtml = popUpHtml + '<option>' + result.data[l].uniTitel + '</option>';
                 }
+                popUpHtml = popUpHtml + '</select></div></div><div class="form-group"><div class="col-md-12"><button id="newBewProzessWahl" class="btn btn-success">Bestätigen</button></div></div></div>';
+                if (popUpHtml.match('<option>') != '<option>') {
+                    popUpHtml = '<b id="popClose"><img src="images/Button Delete.png" id="smallImg"></b><br><p>Sie haben sich bereits für alle verfügbaren Auslandsuniversitäten für ihren Studiengang beworben.</p>';
+                }
+                
                 var popUpHtml2 = '<div class="form-horizontal"><div class="form-group"><div class="col-md-12"><select class="inBox" id="selectUni">';
                 popUpHtml2 = popUpHtml2 + '<option> Zeitraum auswählen </option><option> Wintersemester 2019 </option>';
-                
-                popUpHtml2 = popUpHtml2 + '</select></div></div><div class="form-group"><div class="col-md-12"><button id="newBewProzessWahl" class="btn btn-success">Bestätigen</button></div></div></div>';
-                /*if (popUpHtml.match('<option>') != '<option>') {
-                    popUpHtml = '<b id="popClose"><img src="images/Button Delete.png" id="smallImg"></b><br><p>Sie haben sich bereits für alle verfügbaren Auslandsuniversitäten für ihren Studiengang beworben.</p>';
-                }*/
-                
                 
                 $.sweetModal({
                     title: 'Bitte wähle die Uni aus',
