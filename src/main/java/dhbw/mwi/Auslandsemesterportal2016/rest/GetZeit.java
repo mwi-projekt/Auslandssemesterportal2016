539b6103b8a1be0bf42219b2edbce732e66b55f0
daniels9 <33021140+daniels9@users.noreply.github.com>
2019-06-16 22:18:19 +0200

    Neues Servlet für Abfrage der möglichen Bewerbungszeiträume

---
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/rest/GetZeit.java
@@ -0,0 +1,26 @@
+package dhbw.mwi.Auslandsemesterportal2016.rest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Calendar;
+
+import dhbw.mwi.Auslandsemesterportal2016.db.SQL_queries;
+import dhbw.mwi.Auslandsemesterportal2016.db.Util;
+
+@WebServlet(urlPatterns = { "/unis" })
+public class GetZeit extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter toClient = response.getWriter();
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		toClient.print(year);
+	}
+
+}
