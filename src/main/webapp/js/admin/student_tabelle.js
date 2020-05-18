c82c2866bf3206c21c58e622e08895d6caf9bd6a
NoTeXh <BlackDeath@landing-zone.de>
2020-05-18 15:08:48 +0200

    added edit for student table

--- src/main/webapp/js/admin/student_tabelle.js
+++ src/main/webapp/js/admin/student_tabelle.js
@@ -1,52 +1,42 @@
+$(document).ready(function () {
 
-var rolle = 0;
-var typ = '';
-
-$.ajax({
-    type: "GET",
-    url: baseUrl + "/getUser",
-    data: {
-        rolle: rolle,
-    },
-    success: function (result) {
-        var auslesen = result.data;
-        if (rolle === 3) {
-            var tabelle =
-                '<h2>Registrierte ' + typ + '</h2>' +
-                '<table id="userTable" class="table table-striped table-bordered">' +
-                '<thead>' +
-                '<tr class="titleRow">' +
-                '<th>Vorname</th>' +
-                '<th>Nachname</th>' +
-                '<th>Email</th>' +
-                '<th>Telefonnummer</th>' +
-                '<th>Mobilfunknummer</th>' +
-                '<th></th>' +
-                '<th></th>' +
-                '</tr>' +
-                '</thead>';
-
-            for (var i = 0; i < auslesen.length; i++) {
-                var row = auslesen[i];
-                tabelle = tabelle +
-                    '<tr id="row' + i +
-                    '"><td class="vorname">' +
-                    row.vorname +
-                    '</td><td class="nachname">' +
-                    row.nachname +
-                    '</td><td class="email">' +
-                    row.email +
-                    '</td><td class="telnummer">' +
-                    row.tel +
-                    '</td><td class="mobil">' +
-                    row.mobil +
-                    '</td><td><span class="btn fas fa-edit useredit-button" id="edit' + i +
-                    '" title="Bearbeiten" data-toggle="modal" href="#userEdit"> </span></td>' +
-                    '<td><span class="btn fas fa-trash deleteAAA-button" data-mail="' +
-                    row.email +
-                    '" id="delete' + i +
-                    '" title="Löschen"></span></td></tr>';
+    myTable = $('#example').DataTable({
+        ajax: '/getUser?rolle=3',
+        columns: [
+            {data: 'vorname'},
+            {data: 'nachname'},
+            {data: 'email'},
+            {data: 'tel'},
+            {data: 'mobil'},
+            {data: 'studiengang'},
+            {data: 'kurs'},
+            {data: 'matrikelnummer'},
+            {data: 'standort'},
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<i style="cursor: pointer" id="edit" class="fas fa-edit" data-toggle="modal" data-target="#exampleModal"></i>'
+            },
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<i style="cursor: pointer" id="delete" class="fas fa-trash"></i>'
             }
-        }
-    }
+        ],
+        select: 'single',
+        responsive: true
+    });
+
+    $("#example").on("mousedown", "#edit", function (e) {
+        var myData = myTable.row($(this).parents('tr')).data();
+        $("#vorname").val(myData.vorname);
+        $("#nachname").val(myData.nachname);
+        $("#email").val(myData.email);
+        $("#telefonnummer").val(myData.tel);
+        $("#mobilnummer").val(myData.mobil);
+        $("#studiengang").val(myData.studiengang);
+        $("#kurs").val(myData.kurs);
+        $("#martikelnummer").val(myData.martikelnummer);
+        $("#standort").val(myData.standort);
+    })
 });
