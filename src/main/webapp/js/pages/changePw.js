9c6cba6c50b162777f47df73a8743b68835a7a51
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-05-27 12:17:00 +0200

    added validator to changePw

--- src/main/webapp/js/pages/changePw.js
+++ src/main/webapp/js/pages/changePw.js
@@ -1,11 +1,26 @@
 import {$,baseUrl} from "../config";
 import Swal from "sweetalert2";
 import "bootstrap";
-import "cookieconsent";
+require("jquery-validation")($);
+require("jquery-validation/dist/localization/messages_de.min");
+
+var uuid_var;
+var url;
+
+$.validator.addMethod("pattern", function(value, element, params){
+	console.log(params);
+	let pattern = new RegExp(params);
+	return this.optional(element) || pattern.test(value);
+}, "Ungültige Zeichenfolge.");
 
 $(document).ready(function() {
 	url = new URL(window.location.href);
 	uuid_var = url.searchParams.get("uuid");
+
+	$("#formular").validate({
+		debug: true
+	});
+
 	$(document).on('click', '#savePassword', function() {
 		setpw();
 	})
@@ -15,6 +30,13 @@ function setpw() {
 	var pw_var = $('#pw').val();
 	var pw_repeat = $('#pw_repeat').val();
 	if (pw_var === pw_repeat){
+		var form = $('#formular');
+
+		if (form && !form.valid()) {
+			Swal.fire('Bitte füllen sie alle Felder korrekt aus.');
+			return;
+		}
+
 	$.ajax({
 		type : "POST",
 		url : baseUrl + "/updatePassword",
