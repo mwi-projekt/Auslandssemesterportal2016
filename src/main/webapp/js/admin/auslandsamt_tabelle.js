6d8279bf97256fe17fb1263d265eae90e710504b
NoTeXh <BlackDeath@landing-zone.de>
2020-06-05 12:29:39 +0200

    added edit function for auslandsamt

--- src/main/webapp/js/admin/auslandsamt_tabelle.js
+++ src/main/webapp/js/admin/auslandsamt_tabelle.js
@@ -1,52 +1,114 @@
+$(document).ready(function () {
 
-var rolle = 0;
-var typ = '';
+    const inputVorname = $("#vorname");
+    const inputNachname = $("#nachname");
+    const inputEmail = $("#email");
+    const inputTelefonnummer = $("#telefonnummer");
+    const inputMobilnummer = $("#mobilfunknummer")
 
-$.ajax({
-    type: "GET",
-    url: baseUrl + "/getUser",
-    data: {
-        rolle: rolle,
-    },
-    success: function (result) {
-        var auslesen = result.data;
-        if (rolle === 2) {
-            var tabelle =
-                '<h2>Registrierte ' + typ + '</h2>' +
-                '<table id="userTable" class="table table-striped table-bordered">' +
-                '<thead>' +
-                '<tr class="titleRow">' +
-                '<th>Vorname</th>' +
-                '<th>Nachname</th>' +
-                '<th>Email</th>' +
-                '<th>Telefonnummer</th>' +
-                '<th>Mobilfunknummer</th>' +
-                '<th></th>' +
-                '<th></th>' +
-                '</tr>' +
-                '</thead>';
-
-            for (var i = 0; i < auslesen.length; i++) {
-                var row = auslesen[i];
-                tabelle = tabelle +
-                    '<tr id="row' + i +
-                    '"><td class="vorname">' +
-                    row.vorname +
-                    '</td><td class="nachname">' +
-                    row.nachname +
-                    '</td><td class="email">' +
-                    row.email +
-                    '</td><td class="telnummer">' +
-                    row.tel +
-                    '</td><td class="mobil">' +
-                    row.mobil +
-                    '</td><td><span class="btn fas fa-edit useredit-button" id="edit' + i +
-                    '" title="Bearbeiten" data-toggle="modal" href="#userEdit"> </span></td>' +
-                    '<td><span class="btn fas fa-trash deleteAAA-button" data-mail="' +
-                    row.email +
-                    '" id="delete' + i +
-                    '" title="Löschen"></span></td></tr>';
+    // Erstelle die DataTable
+    myTable = $('#example').DataTable({
+        dom: '<"top"f> rt <"bottom"lp>',
+        language: {
+            processing: "Bitte warten ...",
+            search: "Suchen ",
+            lengthMenu: "_MENU_ Einträge anzeigen",
+            info: "_START_ bis _END_ von _TOTAL_ Einträgen",
+            infoEmpty: "Keine Daten vorhanden",
+            infoFiltered: "(gefiltert von _MAX_ Einträgen)",
+            infoPostFix: "",
+            loadingRecords: "Wird geladen ...",
+            zeroRecords: "Es sind keine Einträge vorhanden.",
+            emptyTable: "Die Tabelle ist leer",
+            paginate: {
+                first: "Erste",
+                previous: "Zurück",
+                next: "Nächste",
+                last: "Letzte"
+            },
+            aria: {
+                sortAscending: ": aktivieren, um Spalte aufsteigend zu sortieren",
+                sortDescending: ": aktivieren, um Spalte absteigend zu sortieren"
             }
+        },
+        ajax: '/getUser?rolle=2',
+        columns: [
+            {data: 'vorname'},
+            {data: 'nachname'},
+            {data: 'email'},
+            {data: 'tel'},
+            {data: 'mobil'},
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<i style="cursor: pointer" id="edit" class="fas fa-edit" data-toggle="modal" data-target="#exampleModal"></i>'
+            },
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<i style="cursor: pointer" id="delete" class="fas fa-trash"></i>'
+            }
+        ],
+        select: 'single',
+        responsive: true
+    });
+
+    let currentEmail;
+
+    // Fülle die Modal-Felder
+    $("#example").on("mousedown", "#edit", function (e) {
+        const myData = myTable.row($(this).parents('tr')).data();
+        inputVorname.val(myData.vorname);
+        inputNachname.val(myData.nachname);
+        currentEmail = myData.email;
+        inputEmail.val(myData.email);
+        inputTelefonnummer.val(myData.tel);
+        inputMobilnummer.val(myData.mobil);
+    });
+
+    // Submit-Button
+    $('#myFormSubmit').click(function (e) {
+        e.preventDefault();
+
+        console.log($('#myForm').serialize());
+
+        var vorname = inputVorname.val();
+        var nachname = inputNachname.val();
+        var email = inputEmail.val();
+        var tel = inputTelefonnummer.val();
+        var mobil = inputMobilnummer.val();
+        var oldMail = "0";
+
+        if (currentEmail != $('#email').val()) {
+            oldMail = currentEmail;
         }
-    }
+
+        Swal.fire({
+            title: 'Speichere Änderungen'
+        });
+        Swal.showLoading();
+        $.ajax({
+            type: "POST",
+            url: baseUrl + "/user/update",
+            data: {
+                email: email,
+                oldmail: oldMail,
+                vorname: vorname,
+                nachname: nachname,
+                tel: tel,
+                mobil: mobil,
+                role: "2"
+            },
+            success: function (result) {
+                Swal.close();
+                Swal.fire('Erfolgreich geändert.', 'Die Benutzerdaten wurden aktualisiert.', 'success');
+                $('#exampleModal').modal('hide');
+            },
+            error: function (result) {
+                Swal.close();
+                Swal.fire('Fehler', 'Es ist ein Fehler beim Aktualisieren aufgetreten. Überprüfen Sie die Eingaben.', 'error');
+                console.log(JSON.stringify(result));
+            }
+        });
+    });
 });
