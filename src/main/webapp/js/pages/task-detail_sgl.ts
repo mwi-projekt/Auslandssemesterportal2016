a055f384a9f1929f79a5c738016e42f3b2d0b104
Tim <43130816+DerStimmler@users.noreply.github.com>
2020-06-08 10:27:58 +0200

    migrated task-detail_sgl.js to typescript

--- src/main/webapp/js/pages/task-detail_sgl.ts
+++ src/main/webapp/js/pages/task-detail_sgl.ts
@@ -2,16 +2,18 @@ import {$,baseUrl} from "../config";
 import "../app";
 import Swal from "sweetalert2";
 import "bootstrap";
+// @ts-ignore
 require("jquery-validation")($);
+// @ts-ignore
 require("jquery-validation/dist/localization/messages_de.min");
 
-var instanceID;
-var url;
-var typeList;
-var verify;
-var idList;
-var sendBew;
-var processDefinition;
+let instanceID : any;
+let url;
+let typeList : any;
+let verify;
+let idList : any;
+let sendBew : any;
+let processDefinition : any;
 
 $(document).ready(function () {
     idList = [];
@@ -20,7 +22,7 @@ $(document).ready(function () {
     instanceID = url.searchParams.get("instance_id");
     verify = url.searchParams.get("verify");
     sendBew = url.searchParams.get("send_bew");
-    var uni = url.searchParams.get("uni");
+    let uni = url.searchParams.get("uni");
     if (!(verify === "true")) {
         if (!(sendBew === "true")) {
             $('#saveChanges').hide();
@@ -44,7 +46,7 @@ $(document).ready(function () {
             uni: uni
         },
         success: function (result) {
-            var step_id = result.active;
+            let step_id = result.active;
             processDefinition = result.data;
             parse();
         }
@@ -53,10 +55,10 @@ $(document).ready(function () {
 });
 
 function createEventListeners(){
-    document.getElementById("saveChanges").addEventListener("click", saveChanges);
-    document.getElementById("validateBtn").addEventListener("click", validateBew);
+    document.getElementById("saveChanges")?.addEventListener("click", saveChanges);
+    document.getElementById("validateBtn")?.addEventListener("click", validateBew);
     $("body").on("change","#validierungErfolgreich",{"param": document.getElementById("validierungErfolgreich")}, (event) => {change(event.data.param)});
-    document.getElementById("backbutton").addEventListener("click", () => location.href='task_overview_sgl.html');
+    document.getElementById("backbutton")?.addEventListener("click", () => location.href='task_overview_sgl.html');
 }
 
 function manipulateDOM() {
@@ -64,7 +66,7 @@ function manipulateDOM() {
 }
 
 function parse() {
-    var output = "";
+    let output = "";
     $.ajax({
         type: "GET",
         url: baseUrl + "/getOverview",
@@ -73,23 +75,24 @@ function parse() {
             definition: processDefinition
         },
         success: function (result) {
-            var steps = result.data;
+            let steps = result.data;
             output = output +
                 '<div class="" id="accordion">';
-            for (var k = 0; k < steps.length; k++) {
-                var data = steps[k].data;
-                var stepName = steps[k].activity;
+            for (let k = 0; k < steps.length; k++) {
+                let data = steps[k].data;
+                let stepName = steps[k].activity;
 
                 if (data.search("id") != -1) {
-                    var innerOutput = "";
-                    var json = JSON.parse(decodeURI(data));
-                    for (var i = 0; i < json.length; i++) {
-                        var type = json[i]["type"];
+                    let innerOutput = "";
+                    let json = JSON.parse(decodeURI(data));
+                    for (let i = 0; i < json.length; i++) {
+                        let type = json[i]["type"];
+                        let req;
                         // alert (type);
                         switch (type) {
                             case "form-select":
-                                var req = "";
-                                var dis = "";
+                                req = "";
+                                let dis = "";
                                 if (json[i]["data"]["required"] == true) {
                                     req = ' required';
                                     dis = ' disabled';
@@ -100,7 +103,7 @@ function parse() {
                                     '</label><div class="col-sm-10"><select class="form-control" id="' +
                                     json[i]["data"]["id"] + '"' + req + dis + ' name="' + json[i]["data"]["id"] + '"' +
                                     '>';
-                                for (var j = 0; j < json[i]["data"]["values"].length; j++) {
+                                for (let j = 0; j < json[i]["data"]["values"].length; j++) {
                                     innerOutput = innerOutput + '<option>' +
                                         json[i]["data"]["values"][j] +
                                         '</option>';
@@ -110,7 +113,7 @@ function parse() {
                                 typeList.push("text");
                                 break;
                             case "form-text":
-                                var req = "";
+                                req = "";
                                 if (json[i]["data"]["required"] == true) {
                                     req = ' required';
                                 }
@@ -137,7 +140,7 @@ function parse() {
                                 break;
                         }
                     }
-                    var visibleStepName;
+                    let visibleStepName;
                     if (innerOutput != '') {
                         if (stepName === "datenEingeben") {
                             visibleStepName = "Persönliche Daten";
@@ -172,18 +175,18 @@ function parse() {
             output = output + '</div></div></div><br>';
 
             output = output + '</div>';
-            document.getElementById("taskDetails").innerHTML = output;
+            document.getElementById("taskDetails")!.innerHTML = output;
 
-            for (var k = 0; k < steps.length; k++) {
-                data = steps[k].data;
-                stepName = steps[k].activity;
+            for (let k = 0; k < steps.length; k++) {
+                let data = steps[k].data;
+                let stepName = steps[k].activity;
                 if (data.search("id") != -1) {
-                    var json = JSON.parse(decodeURI(data));
-                    for (var i = 0; i < json.length; i++) {
-                        var type = json[i]["type"];
+                    let json = JSON.parse(decodeURI(data));
+                    for (let i = 0; i < json.length; i++) {
+                        let type = json[i]["type"];
                         switch (type) {
                             case "form-upload":
-                                var file = json[i];
+                                let file = json[i];
                                 getAccordionFile(file);
                                 break;
                         }
@@ -193,6 +196,7 @@ function parse() {
             getData();
             manipulateDOM();
 
+            // @ts-ignore
             $("#taskDetails").validate({
                 debug: true
             });
@@ -203,7 +207,7 @@ function parse() {
     });
 }
 
-function getAccordionFile(file) {
+function getAccordionFile(file : any) {
     $.ajax({
         type: "HEAD",
         url: baseUrl + "/getProcessFile",
@@ -219,8 +223,8 @@ function getAccordionFile(file) {
 }
 
 function getData() {
-    var keyString = "";
-    for (var l = 0; l < idList.length; l++) {
+    let keyString = "";
+    for (let l = 0; l < idList.length; l++) {
         keyString = keyString + idList[l] + "|";
     }
     keyString = keyString.substr(0, keyString.length - 1);
@@ -233,7 +237,7 @@ function getData() {
             key: keyString
         },
         success: function (result) {
-            $.each(result, function (key, value) {
+            $.each(result, function (key : string, value : any) {
                 $('#' + key).val(value);
                 if(key === 'muttersprache' || key === 'semesteradresseAnders'){
                 	$('#' + key).prop("checked", value);
@@ -250,36 +254,37 @@ function getData() {
     });
 }
 
-function variableEnglishAndSemesteranschrift(key, value){
+function variableEnglishAndSemesteranschrift(key : string, value : any){
 	if(key === 'muttersprache' && value === true){
-		document.getElementById("Englischnote im Abitur in Punkten").remove();
+		document.getElementById("Englischnote im Abitur in Punkten")?.remove();
 	}else if(key === 'semesteradresseAnders' && value === false){
-		document.getElementById("Semesteranschrift").remove();
+		document.getElementById("Semesteranschrift")?.remove();
 	}
 	
 }
 
 function saveChanges() {
-    var form = $('#taskDetails');
+    let form = $('#taskDetails');
 
+    // @ts-ignore
     if (form && !form.valid()) {
         Swal.fire('Bitte füllen sie alle Felder korrekt aus.');
         return;
     }
 
-    var keyString = "";
-    var valString = "";
-    var typeString = "";
-    for (var j = 0; j < idList.length; j++) {
+    let keyString = "";
+    let valString = "";
+    let typeString = "";
+    for (let j = 0; j < idList.length; j++) {
         if ($('#' + idList[j]).attr('type') == 'checkbox') {
-            var checkedString = (document.getElementById(idList[j]).checked) ? 'true' :
+            let checkedString = ((<any>document.getElementById(idList[j])).checked) ? 'true' :
                 'false';
             keyString = keyString + idList[j] + "|";
             valString = valString + checkedString + "|";
             typeString = typeString + typeList[j] + "|";
         } else {
             keyString = keyString + idList[j] + "|";
-            valString = valString + document.getElementById(idList[j]).value + "|";
+            valString = valString + (<any>document.getElementById(idList[j])).value + "|";
             typeString = typeString + typeList[j] + "|";
         }
     }
@@ -330,8 +335,9 @@ function saveChanges() {
 }
 
 function validateBew() {
-    var form = $('#taskDetails');
+    let form = $('#taskDetails');
 
+    // @ts-ignore
     if (form && !form.valid()) {
         Swal.fire('Bitte füllen sie alle Felder korrekt aus.');
         return;
@@ -405,9 +411,9 @@ function validateBew() {
     });
 }
 
-function change(obj) {
-    var selectBox = obj;
-    var selected = selectBox.options[selectBox.selectedIndex].value;
+function change(obj : any) {
+    let selectBox = obj;
+    let selected = selectBox.options[selectBox.selectedIndex].value;
 
     if (selected === '') {
         $('#reason').hide();
