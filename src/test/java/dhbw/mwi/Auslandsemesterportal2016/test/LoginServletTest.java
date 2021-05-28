586b77e7737e0a100ed7d443a7896915c14c0259
Andre Steudel <andre.steudel@yellowmap.de>
2021-05-28 16:51:52 +0200

    add login test

--- src/test/java/dhbw/mwi/Auslandsemesterportal2016/test/LoginServletTest.java
+++ src/test/java/dhbw/mwi/Auslandsemesterportal2016/test/LoginServletTest.java
@@ -1,14 +1,20 @@
 package dhbw.mwi.Auslandsemesterportal2016.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.testng.annotations.Test;
 import dhbw.mwi.Auslandsemesterportal2016.rest.LoginServlet;
+import dhbw.mwi.Auslandsemesterportal2016.db.SQL_queries;
 
 public class LoginServletTest {
       
@@ -20,12 +26,35 @@ public class LoginServletTest {
     */
     @Test
     public void verifyDoPostMethod () throws IOException {
+        // prepare mocks
+        MockedStatic<SQL_queries> sqlMock = Mockito.mockStatic(SQL_queries.class);
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-        LoginServlet loginServlet = mock(LoginServlet.class);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
 
+        // prepare test
+        String email = "testusermwi@dhbw.de";
+        String pw = "12345";
+        String salt = "SH5E9Z7P5J6Z5G2BV0";
+        String studiengang = "WI";
+        String matrikelnummer = "0001";
+        String rolle = "1";
+        String accessToken = "abc123";
+
+        LoginServlet loginServlet = new LoginServlet();
+        when(request.getParameter("email")).thenReturn(email);
+        when(request.getParameter("pw")).thenReturn(pw);
+        sqlMock.when(() -> SQL_queries.getSalt(email)).thenReturn(salt);
+        sqlMock.when(() -> SQL_queries.userLogin(email, salt, pw)).thenReturn(new String[]{
+            "1", studiengang, matrikelnummer, rolle, accessToken
+        });
+
+        // run test
         loginServlet.doPost(request, response);
-        verify(loginServlet,times(1)).doPost(request, response);
+        String result = stringWriter.toString();
+        assertEquals(result, "{\"resultCode\":\"1\",\"studiengang\":\"" + studiengang + "\",\"matrikelnummer\":\"" + matrikelnummer + "\",\"rolle\":\"" + rolle + "\"}");
     }
 
 }
