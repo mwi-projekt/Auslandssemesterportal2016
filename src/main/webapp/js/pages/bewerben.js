afab96762bc32bc8ee7a8e477ea3a4e031e4a752
Timioe <timiioe@gmail.com>
2021-06-28 10:45:33 +0200

    Update bewerben.js

--- src/main/webapp/js/pages/bewerben.js
+++ src/main/webapp/js/pages/bewerben.js
@@ -481,23 +481,9 @@ function parse() {
             });
 
             $("#bewTelefon").keydown(function (e) {
-              if (e.ctrlKey || e.altKey) {
-                e.preventDefault();
-              } else {
+              try {
                 var key = e.keyCode;
-                console.log(key);
-                if (
-                  !(
-                    key == 8 ||
-                    key == 32 ||
-                    key == 46 ||
-                    (key >= 35 && key <= 40) ||
-                    (key >= 65 && key <= 90)
-                  )
-                ) {
-                  e.preventDefault();
-                }
-              }
+              } catch (error) {}
             });
 
             $("#bewNachname").keydown(function (e) {
