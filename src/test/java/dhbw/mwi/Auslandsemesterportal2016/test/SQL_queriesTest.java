94663af17ae8cb9e0b79d4e708b430a46d80c4d0
Karen <karen.s.hahn@web.de>
2022-05-11 18:40:17 +0200

    SQL_queriesTest überarbeiten

--- src/test/java/dhbw/mwi/Auslandsemesterportal2016/test/SQL_queriesTest.java
+++ src/test/java/dhbw/mwi/Auslandsemesterportal2016/test/SQL_queriesTest.java
@@ -3,10 +3,11 @@ package dhbw.mwi.Auslandsemesterportal2016.test;
 import dhbw.mwi.Auslandsemesterportal2016.db.DB;
 import dhbw.mwi.Auslandsemesterportal2016.db.SQL_queries;
 import dhbw.mwi.Auslandsemesterportal2016.db.Util;
-import dhbw.mwi.Auslandsemesterportal2016.enums.TestEnum;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
@@ -15,22 +16,37 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static dhbw.mwi.Auslandsemesterportal2016.enums.TestEnum.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 class SQL_queriesTest {
   private static MockedStatic<SQL_queries> mockedStatic;
+  private Connection connection;
+  private PreparedStatement preparedStatement;
+  private MockedStatic<DB> mockedStaticDb;
 
   @BeforeEach
   public void init() {
     mockedStatic = Mockito.mockStatic(SQL_queries.class);
+    connection = mock(Connection.class);
+    preparedStatement = mock(PreparedStatement.class);
+    mockedStaticDb = Mockito.mockStatic(DB.class);
 
+    mockedStatic.when(() -> SQL_queries.executeUpdate(any(), any(), any())).thenCallRealMethod();
+    mockedStatic.when(() -> SQL_queries.executeStatement(anyString(), any(), any())).thenCallRealMethod();
+    mockedStaticDb.when(DB::getInstance).thenReturn(connection);
+    mockedStatic.when(() -> connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+    mockedStatic.when(preparedStatement::executeUpdate).thenReturn(1);
   }
 
   @AfterEach
-  public void close() {
+  public void close() throws SQLException {
     mockedStatic.close();
+    connection.close();
+    preparedStatement.close();
+    mockedStaticDb.close();
   }
 
   /*
@@ -41,27 +57,106 @@ class SQL_queriesTest {
   @Test
   void testGetSalt() {
 
-    mockedStatic.when(() -> SQL_queries.getSalt(TestEnum.TESTEMAIL.toString())).thenReturn("SH5E9Z7P5J6Z5G2BV0");
-    assertEquals("SH5E9Z7P5J6Z5G2BV0", SQL_queries.getSalt(TestEnum.TESTEMAIL.toString()));
+    mockedStatic.when(() -> SQL_queries.getSalt(TESTEMAIL.toString())).thenReturn("SH5E9Z7P5J6Z5G2BV0");
+    assertEquals("SH5E9Z7P5J6Z5G2BV0", SQL_queries.getSalt(TESTEMAIL.toString()));
 
   }
 
-  /*
-   * Testing static userLogin()-Method using Mockito mocking result-response
-   * 
-   * Method is called in LoginServlet-Class
-   */
   @Test
-  void testUserLogin() {
-    // verifiziert, matrikelnummer, studiengang, rolle, userID
-    String[] stringArr = { "1", TestEnum.TESTSTUGANG.toString(), TestEnum.TESTMATRNR.toString(),
-        TestEnum.TESTROLLESTRING.toString(), "1478523697412" };
+  void getSalt() throws SQLException {
+    mockedStatic.when(() -> SQL_queries.getSalt(TESTEMAIL.toString())).thenCallRealMethod();
 
-    mockedStatic.when(() -> SQL_queries.userLogin(TestEnum.TESTEMAIL.toString(), "SH5E9Z7P5J6Z5G2BV0", "Password1234"))
-        .thenReturn(stringArr);
+    String salt = "anySalt";
+    ResultSet resultSet = mock(ResultSet.class);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(resultSet.next()).thenReturn(true).thenReturn(false);
+    when(resultSet.getString(1)).thenReturn(salt);
 
-    String[] result = SQL_queries.userLogin(TestEnum.TESTEMAIL.toString(), "SH5E9Z7P5J6Z5G2BV0", "Password1234");
-    assertArrayEquals(stringArr, result);
+    String result = SQL_queries.getSalt(TESTEMAIL.toString());
+
+    assertEquals(salt, result);
+  }
+
+  @Test
+  void getSaltThrowsException() throws SQLException {
+    mockedStatic.when(() -> SQL_queries.getSalt(TESTEMAIL.toString())).thenCallRealMethod();
+
+    String salt = "";
+    ResultSet resultSet = mock(ResultSet.class);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(resultSet.next()).thenThrow(RuntimeException.class);
+
+    String result = SQL_queries.getSalt(TESTEMAIL.toString());
+
+    assertEquals(salt, result);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 3})
+  void userLogin(int resultCode) throws SQLException {
+    // given
+    mockedStatic.when(() -> SQL_queries.userLogin(anyString(), anyString(), anyString())).thenCallRealMethod();
+
+    // Data of resultStringArray
+    String studiengang = "anyStudiengang";
+    String matrikelnummer = "anyMatrikelnummer";
+    String rolle = "anyRolle";
+    String accessToken;
+    if (resultCode == 1) {
+      accessToken = "357";
+    } else {
+      accessToken = "";
+    }
+    String[] expected = {("" + resultCode), studiengang, matrikelnummer, rolle, accessToken};
+
+    // Mock Responses
+    ResultSet resultSet = mock(ResultSet.class);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(resultSet.next()).thenReturn(true).thenReturn(false);
+    when(resultSet.getString("studiengang")).thenReturn(studiengang);
+    when(resultSet.getString("matrikelnummer")).thenReturn(matrikelnummer);
+    when(resultSet.getString("rolle")).thenReturn(rolle);
+    when(resultSet.getString("verifiziert")).thenReturn("" + resultCode);
+    when(resultSet.getInt(anyInt())).thenReturn(357);
+    mockedStatic.when(() -> SQL_queries.createUserSession(anyInt())).thenReturn(accessToken);
+
+    // when
+    String salt = "SH5E9Z7P5J6Z5G2BV0";
+    String[] result = SQL_queries.userLogin(TESTEMAIL.toString(), salt, TESTPW.toString());
+
+    // then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void userLoginWrongData() throws SQLException {
+    // given
+    mockedStatic.when(() -> SQL_queries.userLogin(anyString(), anyString(), anyString())).thenCallRealMethod();
+    ResultSet resultSet = mock(ResultSet.class);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(resultSet.next()).thenReturn(false);
+
+    // when
+    String salt = "SH5E9Z7P5J6Z5G2BV0";
+    String[] result = SQL_queries.userLogin(TESTEMAIL.toString(), salt, TESTPW.toString());
+
+    // then
+    String[] expected = {"2", "", "", "", ""};
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void userLoginDbException() throws SQLException {
+    mockedStatic.when(() -> SQL_queries.userLogin(anyString(), anyString(), anyString())).thenCallRealMethod();
+    when(preparedStatement.executeQuery()).thenThrow(RuntimeException.class);
+
+    // when
+    String salt = "SH5E9Z7P5J6Z5G2BV0";
+    String[] result = SQL_queries.userLogin(TESTEMAIL.toString(), salt, TESTPW.toString());
+
+    // then
+    String[] expected = {"4", "", "", "", ""};
+    assertArrayEquals(expected, result);
   }
 
   /*
@@ -77,9 +172,9 @@ class SQL_queriesTest {
     ResultSet resultSet = mock(ResultSet.class);
 
     mockedStatic.when(() -> SQL_queries.executeStatement(any(), any(), any())).thenReturn(resultSet);
-    when(SQL_queries.isEmailUsed(TestEnum.TESTEMAIL.toString())).thenCallRealMethod();
+    when(SQL_queries.isEmailUsed(TESTEMAIL.toString())).thenCallRealMethod();
 
-    assertFalse(SQL_queries.isEmailUsed(TestEnum.TESTEMAIL.toString()));
+    assertFalse(SQL_queries.isEmailUsed(TESTEMAIL.toString()));
   }
 
   /*
@@ -95,10 +190,10 @@ class SQL_queriesTest {
     ResultSet resultSet = mock(ResultSet.class);
 
     mockedStatic.when(() -> SQL_queries.executeStatement(any(), any(), any())).thenReturn(resultSet);
-    when(SQL_queries.isEmailUsed(TestEnum.TESTEMAIL.toString())).thenCallRealMethod();
+    when(SQL_queries.isEmailUsed(TESTEMAIL.toString())).thenCallRealMethod();
     when(resultSet.next()).thenReturn(true);
 
-    assertTrue(SQL_queries.isEmailUsed(TestEnum.TESTEMAIL.toString()));
+    assertTrue(SQL_queries.isEmailUsed(TESTEMAIL.toString()));
   }
 
   /*
@@ -146,31 +241,21 @@ class SQL_queriesTest {
    * 
    */
   @Test
-  void testUserRegister() throws SQLException {
-    Connection connection = mock(Connection.class);
-    PreparedStatement preparedStatement = mock(PreparedStatement.class);
-    MockedStatic<DB> mockedStaticDb = Mockito.mockStatic(DB.class);
-
+  void userRegister() throws SQLException {
     mockedStatic
         .when(() -> SQL_queries.userRegister(anyString(), anyString(), anyString(), anyString(), anyInt(), anyString(),
             anyString(), anyString(), anyInt(), anyString(), anyString(), anyString(), anyString()))
         .thenCallRealMethod();
 
-    mockedStatic.when(() -> SQL_queries.executeUpdate(any(), any(), any())).thenCallRealMethod();
-    mockedStaticDb.when(DB::getInstance).thenReturn(connection);
-    mockedStatic.when(() -> connection.prepareStatement(any())).thenReturn(preparedStatement);
-    mockedStatic.when(preparedStatement::executeUpdate).thenReturn(1);
-
     // vorname, nachname, passwort, salt, rolle, email, studiengang, kurs,
     // matrikelnummer, tel, mobil, standort, verifiziert, resetToken
-    int i = SQL_queries.userRegister(TestEnum.TESTVNAME.toString(), TestEnum.TESTNNAME.toString(), "Test1234",
-        Util.generateSalt(), 1, TestEnum.TESTEMAIL.toString(), TestEnum.TESTSTUGANG.toString(),
-        TestEnum.TESTKURS.toString(), 1234567, TestEnum.TESTTELNR.toString(), TestEnum.TESTMOBILNR.toString(),
-        TestEnum.TESTSTANDORT.toString(), "verifiziert");
+    int i = SQL_queries.userRegister(TESTVNAME.toString(), TESTNNAME.toString(), "Test1234",
+        Util.generateSalt(), 1, TESTEMAIL.toString(), TESTSTUGANG.toString(),
+        TESTKURS.toString(), 1234567, TESTTELNR.toString(), TESTMOBILNR.toString(),
+        TESTSTANDORT.toString(), "verifiziert");
+
     verify(preparedStatement, times(1)).executeUpdate();
     assertEquals(1, i);
-
-    mockedStaticDb.close();
   }
 
 }
