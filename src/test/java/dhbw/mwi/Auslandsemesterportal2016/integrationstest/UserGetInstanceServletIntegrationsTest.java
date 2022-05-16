54b91d26d8910313bc25d3e6a6eea5c199e69ca2
Jessi <jessica.fndr@gmail.com>
2022-05-16 16:52:50 +0200

    Hinzufügen von Integrationstest Sprint 1 und 2

--- src/test/java/dhbw/mwi/Auslandsemesterportal2016/integrationstest/UserGetInstanceServletIntegrationsTest.java
+++ src/test/java/dhbw/mwi/Auslandsemesterportal2016/integrationstest/UserGetInstanceServletIntegrationsTest.java
@@ -8,19 +8,18 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.post;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GetInstanceServletIntegrationsTest {
-
+public class UserGetInstanceServletIntegrationsTest {
     @Test
     void doGetSuccess(){
         Response loginResponse = post("http://10.3.15.45/login?email=test@student.dhbw-karlsruhe.de&pw=Hallo1234!");
         String sessionID = loginResponse.getCookies().get("sessionID");
-        String expected = "{\"instanceId\":\"cd747fc6-d51b-11ec-98d5-0242c0a82003\",\"uni\":\"California State University San Marcos (USA)\"}";
+        String expected = "{\"data\":[{\"instanceID\":\"24850710-d515-11ec-98d5-0242c0a82003\",\"uni\":\"Standard\",\"stepCounter\":\"Schritt 2 von 8\",\"prioritaet\":\"0\"},{\"instanceID\":\"cd747fc6-d51b-11ec-98d5-0242c0a82003\",\"uni\":\"California State University San Marcos (USA)\",\"stepCounter\":\"Schritt 1 von 8\",\"prioritaet\":\"1\"}]}";
 
         String response = given()
                 .cookie("sessionID", sessionID)
                 .cookie("email", "test@student.dhbw-karlsruhe.de")
                 .when()
-                .get("http://10.3.15.45/getInstance?matnr=1901901&uni=California State University San Marcos (USA)&prio=1")
+                .get("http://10.3.15.45/getUserInstances?email=test@student.dhbw.karlsruhe.de&pw=Hallo1234!&matnr=1901901")
                 .then().statusCode(200)
                 .contentType(ContentType.JSON).extract().response().asString();
 
