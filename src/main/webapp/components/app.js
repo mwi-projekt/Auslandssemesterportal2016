902a842044d344ce60db7b2643b0816befc52822
fbubeck <ferdinand.bubeck@gmx.de>
2022-05-30 13:32:05 +0200

    added login-component.js

--- src/main/webapp/components/app.js
+++ src/main/webapp/components/app.js
@@ -1,7 +1,10 @@
-// import faqComponent from "faq-component.js";
 import {faqComponent} from "./faq-component.js";
+import {loginComponent} from "./login-component.js";
 
 const app = Vue.createApp({
-    components: {'faq-component': faqComponent}
+    components: {
+        'faq-component': faqComponent,
+        'login-component': loginComponent
+    }
 })
 .mount('#app')
