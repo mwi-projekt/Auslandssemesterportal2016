50f7146d50403407b9548b8b11b7c30b90f8f466
Karen <karen.s.hahn@web.de>
2022-06-01 10:38:43 +0200

    GetBPMNServletTest geschrieben

--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/FileCreator.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/db/FileCreator.java
@@ -5,7 +5,16 @@ import dhbw.mwi.Auslandsemesterportal2016.Config;
 import java.io.File;
 
 public class FileCreator {
+
+    private FileCreator() {
+    }
+
     public static File getUploadFolder() {
         return new File(Config.UPLOAD_DIR);
+    }
+
+    public static File getBPMNFile(String model) {
+        ClassLoader classLoader = FileCreator.class.getClassLoader();
+        return new File(classLoader.getResource(model + ".bpmn").getFile());
     }
 }
--- src/main/java/dhbw/mwi/Auslandsemesterportal2016/rest/GetBPMNServlet.java
+++ src/main/java/dhbw/mwi/Auslandsemesterportal2016/rest/GetBPMNServlet.java
@@ -1,16 +1,21 @@
 package dhbw.mwi.Auslandsemesterportal2016.rest;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import dhbw.mwi.Auslandsemesterportal2016.db.FileCreator;
+import dhbw.mwi.Auslandsemesterportal2016.db.UserAuthentification;
+import dhbw.mwi.Auslandsemesterportal2016.db.Util;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
-import dhbw.mwi.Auslandsemesterportal2016.db.Util;
-import dhbw.mwi.Auslandsemesterportal2016.db.UserAuthentification;
-import dhbw.mwi.Auslandsemesterportal2016.enums.ErrorEnum;
+import static dhbw.mwi.Auslandsemesterportal2016.enums.ErrorEnum.PARAMMISSING;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
 @WebServlet(name = "GetBPMNServlet", urlPatterns = { "/bpmn/get" })
 public class GetBPMNServlet extends HttpServlet {
@@ -20,49 +25,48 @@ public class GetBPMNServlet extends HttpServlet {
 		Util.addResponseHeaders(request, response);
 
 		int rolle = UserAuthentification.isUserAuthentifiedByCookie(request);
-
 		if (rolle != 1) {
-			response.sendError(401);
-		} else {
-			PrintWriter toClient = response.getWriter();
+			response.sendError(SC_UNAUTHORIZED);
+			return;
+		}
 
-			String model = request.getParameter("model");
+		PrintWriter toClient = response.getWriter();
 
-			final java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("[a-zA-Z\\_\\-]*");
+		String model = request.getParameter("model");
+		final java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("[a-zA-Z\\_\\-]*");
+		// check for File inclusion vulnerability
+		if (model == null || !pattern.matcher(model).matches()) {
+			response.setStatus(SC_INTERNAL_SERVER_ERROR);
+			toClient.println(PARAMMISSING);
+			toClient.close();
+			return;
+		}
 
-			// check for File inclusion vulnerability
-			if (model != null && pattern.matcher(model).matches()) {
+		File file = FileCreator.getBPMNFile(model);
 
-				java.lang.ClassLoader classLoader = getClass().getClassLoader();
-				java.io.File file = new java.io.File(classLoader.getResource(model + ".bpmn").getFile());
+		try {
+			if (file.exists() && !file.isDirectory()) {
+				java.io.FileInputStream fis = new java.io.FileInputStream(file);
+				java.io.BufferedReader reader = new java.io.BufferedReader(
+						new java.io.InputStreamReader(fis, StandardCharsets.UTF_8));
+				response.setContentType("application/octet-stream");
 
-				try {
-					if (file.exists() && !file.isDirectory()) {
-						java.io.FileInputStream fis = new java.io.FileInputStream(file);
-						java.io.BufferedReader reader = new java.io.BufferedReader(
-								new java.io.InputStreamReader(fis, "UTF8"));
-						response.setContentType("application/octet-stream");
-
-						String s = null;
-						while ((s = reader.readLine()) != null) {
-							toClient.println(s);
-						}
-						toClient.close();
-						reader.close();
-					} else {
-						response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-						toClient.println("file not found");
-						toClient.close();
-					}
-				} catch (Exception e) {
-
+				String s;
+				while ((s = reader.readLine()) != null) {
+					toClient.println(s);
 				}
-
+				toClient.close();
+				reader.close();
 			} else {
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				toClient.println(ErrorEnum.PARAMMISSING.toString());
+				response.setStatus(SC_INTERNAL_SERVER_ERROR);
+				toClient.println("file not found");
 				toClient.close();
 			}
+		} catch (Exception e) {
+			response.sendError(SC_INTERNAL_SERVER_ERROR, "file error");
 		}
+
 	}
+
+
 }
