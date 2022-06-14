7926709c43be99378003e7eb060b247c265b6d34
fbubeck <ferdinand.bubeck@gmx.de>
2022-06-14 13:02:56 +0200

    added footer-component.js

--- src/main/webapp/components/app.js
+++ src/main/webapp/components/app.js
@@ -1,7 +1,17 @@
 // import faqComponent from "faq-component.js";
 import {faqComponent} from "./faq-component.js";
+import {footerComponent} from "./footer-component.js";
 
-const app = Vue.createApp({
-    components: {'faq-component': faqComponent}
-})
-.mount('#app')
+const app1 = Vue.createApp({
+    components: {
+        'faq-component': faqComponent
+    },
+});
+app1.mount('#vue-faq');
+
+const app2 = Vue.createApp({
+    components: {
+        'footer-component': footerComponent,
+    },
+});
+app2.mount('#vue-layout');
